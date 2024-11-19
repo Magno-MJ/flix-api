9f55163ab456c2c614e352298d69fb2ba7bee747
Magno Jovino <magno.jovino@hotmail.com>
2024-11-19 19:59:37 -0300

    feat: adiciona módulo de gêneros

---
+++ genres/views.py
@@ -0,0 +1,16 @@
+from genres.models import Genre
+from rest_framework import generics
+from genres.serializers import GenreSerializer
+from rest_framework.permissions import IsAuthenticated
+from app.permissions import GlobalDefaultPermissionClass
+
+class GenreCreateListView(generics.ListCreateAPIView):
+  permission_classes = (IsAuthenticated, GlobalDefaultPermissionClass)
+  queryset = Genre.objects.all()
+  serializer_class = GenreSerializer
+
+
+class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes = (IsAuthenticated, GlobalDefaultPermissionClass)
+  queryset = Genre.objects.all()
+  serializer_class = GenreSerializer
